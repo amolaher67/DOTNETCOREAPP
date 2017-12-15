1009912d5d358de04235593aca73a2713518cc7e
Amol Aher <amola@WinjitTechnologiesPvtLtd.local>
2017-12-15 16:04:37 +0530

    Add project files.

---
+++ CoreWebAppTest/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAppTest.Models
+{
+    public class MovieContext:DbContext
+    {
+        public MovieContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
